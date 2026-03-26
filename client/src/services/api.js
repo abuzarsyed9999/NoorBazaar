c7bb02e22ad818750b5300586a2c8ee520fba639
abuzarsyed9999 <abuzarcda@gmail.com>
2026-03-26 19:20:32 +0530

    fix: vercel routing + updates

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: false, // change to true if your backend requires cookies
   headers: {
     "Content-Type": "application/json",
   },
